985488474512f41d3fedda9ac7f56472d6f80b76
sayantanHack <sayantandatta97@gmail.com>
2018-10-12 20:59:32 +0530

    function calling changed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -12,21 +12,21 @@
    var entriesStart,
         entriesEnd;
 
-   describe('RSS Feeds',() =>{
+   describe('RSS Feeds',function() {
 
-     it('feeds are defined', ()=> {
+     it('feeds are defined', function() {
        expect(allFeeds).toBeDefined(;
        expect(allFeeds.length).not.toBe(0));
      });
 
-     it('URLs are defined',()=>{
+     it('URLs are defined',function(){
         for (var i = 0; i < allFeeds.length; i++) {
           expect(allFeeds[i].url).toBeDefined();
           expect(allFeeds[i].url.length).not.toBe(0);
         };
      });
 
-     it('names are defined',()=> {
+     it('names are defined',function() {
        for (var i = 0; i < allFeeds.length; i++) {
           expect(allFeeds[i].name).toBeDefined();
           expect(allFeeds[i].name.length).not.toBe(0);
@@ -35,13 +35,13 @@
 
    });
 
-   describe('The Menu', () => {
+   describe('The Menu', function() {
 
-     it('menu element is hidden', ()=>{
+     it('menu element is hidden', function(){
        expect($('body').hasClass('menu-hidden')).toEqual(true);
      });
 
-     it('working toggle on click event', ()=>{
+     it('working toggle on click event', function(){
        $('.menu-icon-link').trigger('click');
        expect($('body').hasClass('menu-hidden')).toBe(false);
        $('.menu-icon-link').trigger('click');
@@ -50,34 +50,34 @@
 
    });
 
-   describe('Initial Entries', ()=>{
+   describe('Initial Entries', function(){
       beforeEach((done) => {
-        loadFeed(0, ()=>{
+        loadFeed(0, function(){
           done();
         });
       });
 
-      it('define if entry has more than 0 entries', ()=>{
+      it('define if entry has more than 0 entries', function(){
         expect($('.entry .feed')).toBeDefined();
       });
 
    });
 
-   describe('New Feed Selection',()=>{
+   describe('New Feed Selection',function(){
      beforeEach((done)=>{
        $('.feed').empty();
-       loadFeed(0, ()=>{
+       loadFeed(0, function(){
          entriesStart = $('.feed').find(allFeeds.url);
          done();
        });
 
-       loadFeed(1,()=>{
+       loadFeed(1,function(){
          entriesEnd = $('.feed').find(allFeeds.url);
          done();
        });
      });
 
-     it('new feed is different to old one',()=>{
+     it('new feed is different to old one',function(){
        expect(entriesStart).not.toBe(entriesEnd);
      });
 
